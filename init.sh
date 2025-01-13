0bfcef93b5c5209cfd266e0d7126c89bbae96659
Pragash Sasitharan <Pragash1001@gmail.com>
2025-01-13 14:28:54 +0000

    Updated Initialising file to apply the RBAC for AKS and run ansible without host key checking

--- init.sh
+++ init.sh
@@ -15,6 +15,9 @@ fi
 # Initialize the terraform
 terraform init
 
+# Destroy the existing resources
+terraform destroy -auto-approve
+
 # Apply the terraform
 terraform apply -auto-approve
 
@@ -24,11 +27,31 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Get the IP address of the VM
-inventory=$(terraform output ansible_inventory)
+# Get the IP address of the VM and remove any quotes
+inventory=$(terraform output -raw ansible_inventory)
 
 # Go to the ansible directory
 cd ../ansible
 
+# Write the inventory to a file
+echo "$inventory" > inventory.ini
+
 # Run the ansible playbook
-ansible-playbook -i $inventory playbook.yml
+ansible-playbook -i inventory.ini playbook.yml --ssh-common-args='-o StrictHostKeyChecking=no'
+
+if [ $? -ne 0 ]; then
+    echo "Error occurred while running the ansible playbook"
+    exit 1
+fi
+
+# Cluster Configuration
+cd ../cluster_config
+
+kubectl create ns webapps
+kubectl apply -f service-account.yml
+kubectl apply -f role.yml
+kubectl apply -f rolebinding.yml
+kubectl apply -f sec.yml -n webapps
+k8_token=$(kubectl describe secret $(kubectl get secret -n webapps | grep webapps-token | awk '{print $1}') -n webapps | grep token: | awk '{print $2}')
+echo $k8_token > k8s_token.txt
+echo "Add the k8_token to Jenkins credentials with the ID 'k8-token'"
