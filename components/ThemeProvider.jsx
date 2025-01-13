1a01e4d21cfea5bd524280c362f46454bcab917d
Pragash Sasitharan <Pragash1001@gmail.com>
2025-01-13 07:10:59 +0000

    [FIX] Error

--- components/ThemeProvider.jsx
+++ components/ThemeProvider.jsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { ThemeProvider as NextThemeProvider } from 'next-themes';
-'next-themes';
 
 export function ThemeProvider({ children, ...props }) {
   return <NextThemeProvider { ...props }>{children}</NextThemeProvider>;
