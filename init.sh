ea1b2800ad01a6aca8e123fafdf7485607d56eb7
Pragash Sasitharan <Pragash1001@gmail.com>
2025-01-13 15:33:29 +0000

    Updated Initialising Script to download the kube config and configure the AKS

--- init.sh
+++ init.sh
@@ -4,6 +4,9 @@
 # Go to the terraform directory
 cd terraform
 
+# Initialize the terraform
+terraform init
+
 # Validate the terraform
 terraform validate
 
@@ -11,9 +14,6 @@ if [ $? -ne 0 ]; then
     echo "Error occurred while validating the terraform"
     exit 1
 fi
-
-# Initialize the terraform
-terraform init
 
 # Destroy the existing resources
 terraform destroy -auto-approve
@@ -27,8 +27,39 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Get the IP address of the VM and remove any quotes
+# Get output values from terraform
+subscription_id=$(terraform output -raw subscription_id)
 inventory=$(terraform output -raw ansible_inventory)
+aks_name=$(terraform output -raw aks_cluster_name)
+aks_rg=$(terraform output -raw aks_cluster_rg)
+
+# Get Kube Config
+az account set --subscription $subscription_id
+az aks get-credentials --resource-group $aks_rg --name $aks_name --overwrite-existing
+
+if [ $? -ne 0 ]; then
+    echo "Error occurred while getting the kube config"
+    exit 1
+fi
+
+# Cluster Configuration
+cd ../cluster_config
+
+kubectl create ns webapps
+kubectl apply -f service-account.yml
+kubectl apply -f role.yml
+kubectl apply -f rolebinding.yml
+kubectl apply -f sec.yml -n webapps
+
+if [ $? -ne 0 ]; then
+    echo "Error occurred while creating the cluster configuration"
+    exit 1
+fi
+
+k8_token=$(kubectl describe secret token-jenkins -n webapps | grep "token:" | awk '{print $2}')
+echo $k8_token > k8s_token.txt
+echo "Add the k8_token to Jenkins credentials with the ID 'k8-token'"
+
 
 # Go to the ansible directory
 cd ../ansible
@@ -43,15 +74,3 @@ if [ $? -ne 0 ]; then
     echo "Error occurred while running the ansible playbook"
     exit 1
 fi
-
-# Cluster Configuration
-cd ../cluster_config
-
-kubectl create ns webapps
-kubectl apply -f service-account.yml
-kubectl apply -f role.yml
-kubectl apply -f rolebinding.yml
-kubectl apply -f sec.yml -n webapps
-k8_token=$(kubectl describe secret $(kubectl get secret -n webapps | grep webapps-token | awk '{print $1}') -n webapps | grep token: | awk '{print $2}')
-echo $k8_token > k8s_token.txt
-echo "Add the k8_token to Jenkins credentials with the ID 'k8-token'"
